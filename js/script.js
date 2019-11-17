4f39bb94973dc62f95d2a864140126cf918f3fdf
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2019-11-17 18:11:06 +0100

    Create Hangman Game according to the instruction (u03-hangman)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,175 @@
-// Globala variabler
+// (1) Definere globala variabler
 
-const wordList;      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+// Skapa array med spelets alla ord:
+const wordList = [ 
+  "Pippi", 
+  "Emil", 
+  "Annika", 
+  "Tommy", 
+  "Astrid", 
+  "Lindgren", 
+  "Mio", 
+  "Karlsson", 
+  "Ida"
+];
 
-let guesses = 0;     // Number: håller antalet gissningar som gjorts
-let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+// Definiera lite ord som spelaren ska gissa på i arrayen wordList. 
+// Sträng: ett av orden valt av en slumpgenerator från arrayen ovan.
+let selectedWord;
 
-let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
-let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+// Number: håller antalet gissningar som gjorts
+let guesses; 
+const guessTries = 6;
 
-// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som slumpar fram ett ord
-// Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
-// Funktion som körs när du trycker på bokstäverna och gissar bokstav
-// Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
-// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+// Nummer av letter som spelaren har gissat
+let countLettersFound;
+
+// Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+let hangmanImg = document.getElementById("hangman");
+
+// DOM-nod: Ger meddelande när spelet är över.
+let msgHolderEl = document.getElementById("message"); 
+
+
+// (2) Använd document.querySelector() för att hämta knappen "Starta Spelet" i DOM, lagra den i startGameBtnEl variabeln.
+// DOM-nod: knappen som du startar spelet med
+let startGameBtnEl = document.querySelector("#startGameBtn");
+console.log(startGameBtnEl);
+
+let letterButtonEls = document.querySelectorAll("#letterButtons > li > button"); // Array av DOM-noder: Knapparna för bokstäverna
+let letterBoxEls = document.querySelector("#letterBoxes > ul"); // Array av DOM-noder: Rutorna där bokstäverna ska stå
+
+
+// (3) Skapa en event-lyssnare för knappen (.addEventListener('click', callbackFn)).
+// Skapa en event-lyssnare (attachEvent) för EI
+if (startGameBtnEl.addEventListener) {
+  startGameBtnEl.addEventListener("click", startButtonClick);
+}
+else if (startGameBtnEl.attachEvent) {
+  startGameBtnEl.attachEvent("onclick", startButtonClick);
+}
+
+// (4) Skapa funktioner
+
+function startButtonClick() {
+  selectedWord = generateWord();
+  createLetterBoxes();
+  console.log("Game started with word: " + selectedWord);
+
+  countLettersFound = 0;
+  guesses = 0;
+  // Activera letter buttons när spelet startar
+  enableLetterButtons();
+  printMessage(""); // Clear message
+  hangmanImg.src = "images/h0.png";
+}
+
+function generateWord() {
+  let index = Math.floor(Math.random() * wordList.length);
+  return wordList[index].toUpperCase();
+}
+
+function createLetterBoxes() {
+  // Ta bort letter box 
+  while (letterBoxEls.firstChild) {
+    letterBoxEls.removeChild(letterBoxEls.firstChild);
+  }
+
+  // Lägg  valt ord till letter boxes 
+  for (let letterNum = 0; letterNum < selectedWord.length; letterNum++) {
+    let item = document.createElement("li");
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = " ";
+    input.disabled = true;
+    item.appendChild(input); // Append "input" to item
+    letterBoxEls.appendChild(item); // Append item to "ul"
+  }
+}
+
+// Skapa en callback-funktion startGame() för event-lyssnaren, denna funktion ska starta spelet. Det gör den genom att ropa på andra funktioner:
+
+// Lyssna på klick på alla bokstavsknappar
+for (let buttonNum = 0; buttonNum < letterButtonEls.length; buttonNum++) {
+  if (letterButtonEls[buttonNum].addEventListener) {
+    letterButtonEls[buttonNum].addEventListener("click", startletterButton);
+  }
+  else if (letterButtonEls[buttonNum].attachEvent) {
+    letterButtonEls[buttonNum].attachEvent("onclick", startletterButton);
+  }
+}
+
+// Array av DOM-noder: Knapparna för bokstäverna
+function startletterButton(event) {
+  let button = event.target || event.srcElement;
+  console.log("Clicked button " + button.value);
+
+  let match = false;
+  for (let letterNum = 0; letterNum < selectedWord.length; letterNum++) {
+    if (selectedWord.charAt(letterNum) == button.value) {
+      console.log("Letter matched on position " + letterNum);
+      match = true;
+      break;
+    }
+  }
+
+  if (match) {
+    handleMatch(button);
+  } else {
+    handleNoMatch();
+  }
+}
+
+function handleMatch(buttonEl) {
+  console.log("Match");
+
+  buttonEl.disabled = true;
+  for (let letterNum = 0; letterNum < selectedWord.length; letterNum++) {
+    if (selectedWord.charAt(letterNum) == buttonEl.value) {
+      letterBoxEls.childNodes[letterNum].querySelector("input").value = selectedWord[letterNum];
+      countLettersFound++;
+    }
+  }
+
+  console.log("countLettersFound: " + countLettersFound);
+  if (countLettersFound == selectedWord.length) {
+    printMessage("Du är vinnare! Starta spelet igen?");
+    prepareForPlaying();
+  }
+}
+
+function handleNoMatch() {
+  console.log("No match");
+
+  guesses++;
+  hangmanImg.src = "images/h" + guesses + ".png";
+
+  if (guesses == guessTries) {
+    printMessage("Du har förlorat. Starta spelet igen?");
+    prepareForPlaying(); 
+  }
+}
+
+function printMessage(message) {
+  let h3El = msgHolderEl.querySelector("h3");
+  h3El.textContent = message;
+}
+
+function prepareForPlaying() {
+  disableLetterButtons();
+}
+
+function enableLetterButtons() {
+  for (let buttonNum = 0; buttonNum < letterButtonEls.length; buttonNum++) {
+    letterButtonEls[buttonNum].disabled = false;
+  }
+}
+
+function disableLetterButtons() {
+  for (let buttonNum = 0; buttonNum < letterButtonEls.length; buttonNum++) {
+    letterButtonEls[buttonNum].disabled = true;
+  }
+}
+
+// Called on reload
+prepareForPlaying();
